2491f7775154e023b7e6ba8935437ad95479ec0c
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-06-28 11:57:03 +0200

    Finish GPX export

--- app/src/main/java/com/example/compass/ListActivity.kt
+++ app/src/main/java/com/example/compass/ListActivity.kt
@@ -2,12 +2,13 @@ package com.example.compass
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,9 +16,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_list.*
+import java.io.FileOutputStream
+import java.io.PrintWriter
 
 class ListActivity : AppCompatActivity() {
     private lateinit var dataPointViewModel: DataPointViewModel
+    private val newDataPointActivityRequestCode = 1
+    private val pickExportFile = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,16 +63,17 @@ class ListActivity : AppCompatActivity() {
         super.onOptionsItemSelected(item)
 
         if (item.title == getString(R.string.action_export)) {
-            Toast.makeText(this, "Exporting data", Toast.LENGTH_SHORT).show()
-
-            // TODO: export data
-            val dataPoints = dataPointViewModel.allDataPoints.value!!
-            Log.v("Export", dataPoints[0].getXml())
+            val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "application/gpx"
+                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                putExtra(Intent.EXTRA_TITLE, "trace.gpx")
+            }
+            startActivityForResult(intent, pickExportFile)
         }
         return true
     }
 
-    private val newDataPointActivityRequestCode = 1
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -84,6 +90,33 @@ class ListActivity : AppCompatActivity() {
                     .show()
             }
 
+        }
+
+        if (requestCode == pickExportFile) {
+            Log.v("Export", "Got response from file picker")
+            val uri = data?.data
+            if (uri == null) {
+                Log.e("Export", "Did not receive a URI")
+                return
+            }
+
+            val dataPoints = dataPointViewModel.allDataPoints.value!!
+            val content = XmlBuilder.buildTrackFromPoints(dataPoints)
+            val export = XmlBuilder.buildFullTrack(
+                "visualizing_the_databike",
+                "Generation Test Track",
+                "Zesty",
+                0,
+                content
+            )
+
+            contentResolver.openFileDescriptor(uri, "w")?.use{ parcelFileDescriptor ->
+                FileOutputStream(parcelFileDescriptor.fileDescriptor).use{ fileOutputStream ->
+                    PrintWriter(fileOutputStream).use {printWriter ->
+                        printWriter.print(export)
+                    }
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/compass/XmlBuilder.kt
@@ -0,0 +1,111 @@
+package com.example.compass
+
+import kotlin.text.StringBuilder
+
+object XmlBuilder {
+    //TODO: clean this up
+    @JvmStatic
+    fun buildFullTrack(creator: String, name: String, bikeName: String, pedalOffset: Int, content: String): String {
+        val builder = StringBuilder()
+
+        builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        builder.append("<gpx version=\"1.1\" creator=\"$creator\">\n")
+        builder.append("<metadata>\n")
+        builder.append("\t<name>$name</name>\n")
+        builder.append("\t<bikename>$bikeName</bikename>\n")
+        builder.append("\t<pedalOffset>$pedalOffset</pedalOffset>\n")
+        builder.append("</metadata>\n")
+        builder.append("$content")
+        builder.append("</gpx>")
+        return builder.toString()
+    }
+
+    @JvmStatic
+    fun buildTrackFromSegments(segments: List<List<DataPoint>>): String {
+        val builder = StringBuilder()
+
+        builder.append("<trk>\n")
+        for (segment in segments)
+            builder.append(buildTrackSegment(segment))
+        builder.append("</trk>\n")
+
+        return builder.toString()
+    }
+
+    @JvmStatic
+    fun buildTrackFromPoints(dataPoints: List<DataPoint>): String {
+        val builder = StringBuilder()
+
+        builder.append("<trk>\n")
+        builder.append(buildTrackSegment(dataPoints))
+        builder.append("</trk>\n")
+
+        return builder.toString()
+    }
+
+    @JvmStatic
+    fun buildTrackSegment(dataPoints: List<DataPoint>): String {
+        val builder = StringBuilder()
+
+        builder.append("<trkseg>\n")
+
+        for (dataPoint in dataPoints) {
+            builder.append(fromDataPoint(dataPoint))
+        }
+
+        builder.append("</trkseg>\n")
+        return builder.toString()
+    }
+
+    fun fromDataPoint(dataPoint: DataPoint): String {
+        // dummy values for now
+        val lat = 47.263308
+        val lon = 11.353000
+        val elevation = 500
+        val time = "2020-04-17T14:20:00.000"
+        val addRotX = 0
+        val addRotY = 0
+        val addRotZ = 0
+        val wheelRpm = 0
+        val steeringRot = 0
+        val pedalRot = 0
+        val pedalRotDir = 1
+        val gearFront = 1
+        val gearRear = 1
+        val brakeFront = 0
+        val brakeRear = 0
+        val suspensionFront = 0
+        val suspensionRear = 0
+        val seatPosition = 0
+
+        val builder = StringBuilder()
+
+        builder.append("<trkpt lat=\"$lat\" lon=\"$lon\">\n")
+        builder.append("\t<ele>$elevation</ele>\n")
+        builder.append("\t<time>$time</time>\n")
+        builder.append("\t<extensions>\n")
+
+        builder.append("\t\t<absrotx>${dataPoint.absRotX}</absrotx>\n")
+        builder.append("\t\t<absroty>${dataPoint.absRotY}</absroty>\n")
+        builder.append("\t\t<absroty>${dataPoint.absRotY}</absroty>\n")
+        builder.append("\t\t<addrotx>$addRotX</addrotx>\n")
+        builder.append("\t\t<addroty>$addRotY</addroty>\n")
+        builder.append("\t\t<addrotz>$addRotZ</addrotz>\n")
+        builder.append("\t\t<wheelrpm>$wheelRpm</wheelrpm>\n")
+        builder.append("\t\t<steeringrot>$steeringRot</steeringrot>\n")
+        builder.append("\t\t<pedalrot>$pedalRot</pedalrot>\n")
+        builder.append("\t\t<pedalrotdir>$pedalRotDir</pedalrotdir>\n")
+        builder.append("\t\t<gearfront>$gearFront</gearfront>\n")
+        builder.append("\t\t<gearrear>$gearRear</gearrear>\n")
+        builder.append("\t\t<brakerear>$brakeRear</brakerear>\n")
+        builder.append("\t\t<brakefront>$brakeFront</brakefront>\n")
+        builder.append("\t\t<suspfront>$suspensionFront</suspfront>\n")
+        builder.append("\t\t<susprear>$suspensionRear</susprear>\n")
+        builder.append("\t\t<seatpos>$seatPosition</seatposition>\n")
+
+        builder.append("\t</extensions>\n")
+        builder.append("</trkpt>\n")
+
+        return builder.toString()
+    }
+}
